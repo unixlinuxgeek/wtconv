0e46a707a52beb463b7c353321f64180a9e2dceb
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-05-05 19:37:38 +0500

    Added func

--- wtconv_test.go
+++ wtconv_test.go
@@ -7,12 +7,24 @@ import (
 
 // KgToLb Умножаем значение массы на 2.205
 func TestKgToLb(t *testing.T) {
-	kg := floatgen.GenRan(1, 9)
+	kg := Kg(floatgen.GenRan(1, 9))
 	lb := Lb(kg * 2.205)
 
-	if lb == KgToLb(Kg(kg)) {
+	if lb == KgToLb(kg) {
 		t.Logf("TestKgToLb Passed!!!: %v equal %v\n", kg, lb)
 	} else {
-		t.Fatalf("TestKgToLb Failed: %v no equal %v\n", kg, lb)
+		t.Fatalf("TestKgToLb Failed: %v not equal %v\n", kg, lb)
+	}
+}
+
+// LbToKg Разделим значение массы на 2.205
+func TestLbToKg(t *testing.T) {
+	lb := Lb(floatgen.GenRan(1, 9))
+	kg := Kg(lb / 2.205)
+
+	if kg == LbToKg(lb) {
+		t.Logf("TestLbToKg Passed!!!: %v equal %v\n", lb, kg)
+	} else {
+		t.Fatalf("TestLbToKg Failed: %v not equal %v\n", lb, kg)
 	}
 }
