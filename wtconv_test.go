44b0da1cb2e9d12f5218c3e99bc9fea85c671ed3
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-05-05 19:23:55 +0500

    Added Test Method

--- wtconv_test.go
+++ wtconv_test.go
@@ -1,1 +1,18 @@
 package wtconv
+
+import (
+	"github.com/unixlinuxgeek/floatgen"
+	"testing"
+)
+
+// KgToLb Умножаем значение массы на 2.205
+func TestKgToLb(t *testing.T) {
+	kg := floatgen.GenRan(1, 9)
+	lb := Lb(kg * 2.205)
+
+	if lb == KgToLb(Kg(kg)) {
+		t.Logf("TestKgToLb Passed!!!: %v equal %v\n", kg, lb)
+	} else {
+		t.Fatalf("TestKgToLb Failed: %v no equal %v\n", kg, lb)
+	}
+}
